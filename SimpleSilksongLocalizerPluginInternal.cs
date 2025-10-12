d407072369357350fd624684875d3ef1d732dcc4
capitalistspz <keipitalists@proton.me>
2025-10-12 05:52:31 +0100

    Automatically search plugin subdirectories for Language subdirectory

--- SimpleSilksongLocalizerPluginInternal.cs
+++ SimpleSilksongLocalizerPluginInternal.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 using TeamCherry.Localization;
 
@@ -11,15 +13,32 @@ public partial class SimpleSilksongLocalizerPlugin : BaseUnityPlugin
     internal static List<string> ModLanguageDirectories = [];
     internal static List<string> ModRegisteredSheetTitles = [];
     internal static Dictionary<LanguageCode, Dictionary<string,Dictionary<string, string>>> ModExtraEntries = new ();
-    
+
+    public static new ManualLogSource Logger;
     private Harmony harmony;
     private string[] originalSheetTitles;
     
     private void Awake()
     {
+        Logger = base.Logger;
+
         harmony = new Harmony(Id);
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
     }
+
+    private void FindDirectories()
+    {
+        var pluginDir = new DirectoryInfo(BepInEx.Paths.PluginPath);
+        foreach (var pluginSubDir in pluginDir.EnumerateDirectories("*", SearchOption.TopDirectoryOnly))
+        {
+            foreach (var languageDir in pluginSubDir.EnumerateDirectories("Language", SearchOption.TopDirectoryOnly))
+            {
+                Logger.LogInfo($"Found language directory in {pluginSubDir.Name}");
+                ModLanguageDirectories.Add(languageDir.FullName);
+            }
+        }
+    }
+    
     private void Start()
     {
         // For initial load, present because patching `Language` at `Awake` or `OnEnable` causes a crash due to its static initializer
@@ -29,7 +48,7 @@ public partial class SimpleSilksongLocalizerPlugin : BaseUnityPlugin
     private void OnEnable()
     {
         // For reloads
-        if (!didStart)
+        if (!didStart || harmony.GetPatchedMethods().Any())
             return;
         Apply();
     }
@@ -41,6 +60,8 @@ public partial class SimpleSilksongLocalizerPlugin : BaseUnityPlugin
 
     private void Apply()
     {
+        FindDirectories();
+
         foreach (var line in ModLanguageDirectories
                      .Select(dir => Path.Combine(dir, "CustomSheetTitles.txt"))
                      .Where(File.Exists)
@@ -62,6 +83,7 @@ public partial class SimpleSilksongLocalizerPlugin : BaseUnityPlugin
     private void Unapply()
     {
         harmony.UnpatchSelf();
+        ModLanguageDirectories.Clear();
         
         Language._settings.sheetTitles = originalSheetTitles;
         Language.LoadAvailableLanguages();
